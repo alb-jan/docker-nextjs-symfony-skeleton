45642fa641b5461e815bb2d8a16fbd96b96cda92
jal <jal@hayloft-it.ch>
2025-09-02 14:44:36 +0200

    fix git collisions

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
     return (
         <div className="h-screen w-screen relative">
-            <p className="text-lg">Hello Worlddd!</p>
+            <p className="text-lg">Hello World!</p>
         </div>
     );
 }
